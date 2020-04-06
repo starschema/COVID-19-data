579850dbddc8520a6c4b9a212c61a0c2f21a5516
william-wash <53308387+william-wash@users.noreply.github.com>
2020-04-06 17:28:32 -0400

    Updated missing data check

--- snowflake/qa/JHU_COVID-19_QA.sql
+++ snowflake/qa/JHU_COVID-19_QA.sql
@@ -1,13 +1,13 @@
-with err (fips, date, err_count) AS
+with err AS
 (
-select count(*)
+select count(*) as err_count
 from COVID19.PUBLIC.JHU_COVID_19
-having count(*) > 1
+having count(*) = 0
 )
 SELECT 
 'JHU_COVID_19' as table_name
-,'Duplicate data' as error_desc
-,'select fips, date, count(*) from JHU_COVID_19 where JHU_COVID_19.ISO3166_1 = "US"  and CASE_TYPE = "Confirmed"  and JHU_COVID_19.County is not null  and JHU_COVID_19.fips is not nullgroup by 1, 2 having count(*) > 1' as error_condition
+,'No records in table' as error_desc
+,'select count(*) as err_count from COVID19.PUBLIC.JHU_COVID_19 having count(*) = 0' as error_condition
 ,sum(err_count) as error_count
 FROM
 err
